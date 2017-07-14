d427a103b29207052edc7133c507ba9424b7a6a7
truebucha <truebucha@gmail.com>
2017-07-14 09:01:54 +0300

    allow user see fields under keyboard

--- iotSensorClient/SettingsViewController.swift
+++ iotSensorClient/SettingsViewController.swift
@@ -11,8 +11,9 @@ import UIKit
 
 
 class SettingsViewController: UIViewController {
-  @IBOutlet weak var composerDataRequestUrlTitleLabel: UILabel!
+  @IBOutlet weak var contentScrollView: UIScrollView!
   
+  @IBOutlet weak var composerDataRequestUrlTitleLabel: UILabel!
   @IBOutlet weak var composerUrlValueLabel: UILabel!
   
   @IBOutlet weak var sensorAddressTitleLabel: UILabel!
@@ -22,6 +23,7 @@ class SettingsViewController: UIViewController {
   @IBOutlet weak var ipAddressField: UITextField!
   @IBOutlet weak var portLabel: UILabel!
   @IBOutlet weak var portField: UITextField!
+  
   @IBOutlet weak var dataRequestTitleLabel: UILabel!
   @IBOutlet weak var urlPathLabel: UILabel!
   @IBOutlet weak var urlPathField: UITextField!
@@ -30,18 +32,58 @@ class SettingsViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-      // Do any additional setup after loading the view, typically from a nib
+    
     self.navigationController?.isNavigationBarHidden = false
   }
 
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
     
+    subscribeToNotifications()
+    
     Settings.shared.updateRouterIp()
   }
   
 
-   override func viewDidDisappear(_ animated: Bool) {
-      super.viewDidDisappear(animated)
+  override func viewDidDisappear(_ animated: Bool) {
+    super.viewDidDisappear(animated)
+  
+    unsubscribeFromAllNotifications()
   }
+  
+  // MARK: notifications 
+  
+  func subscribeToNotifications() {
+    NotificationCenter.default.addObserver(self,
+                                           selector: #selector(keyboardWillShow),
+                                           name: .UIKeyboardWillShow,
+                                           object: nil)
+    NotificationCenter.default.addObserver(self,
+                                           selector: #selector(keyboardWillHide),
+                                           name: .UIKeyboardWillHide,
+                                           object: nil)
+  }
+  
+  func unsubscribeFromAllNotifications() {
+    NotificationCenter.default.removeObserver(self)
+  }
+  
+  // MARK: scroll view + keyboard logic
+  
+  func keyboardWillShow(notification:NSNotification){
+    //give room at the bottom of the scroll view, so it doesn't cover up anything the user needs to tap
+    var userInfo = notification.userInfo!
+    var keyboardFrame = (userInfo[UIKeyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+    keyboardFrame = self.view.convert(keyboardFrame, from: nil)
+
+    var contentInset = contentScrollView.contentInset
+    contentInset.bottom = keyboardFrame.size.height + 16.0
+    contentScrollView.contentInset = contentInset
+  }
+
+  func keyboardWillHide(notification:NSNotification){
+    let contentInset:UIEdgeInsets = UIEdgeInsets.zero
+    contentScrollView.contentInset = contentInset
+  }
+  
 }
